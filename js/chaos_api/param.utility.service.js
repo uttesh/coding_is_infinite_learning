5b363a352926754bc1d83c8e09574618cf749245
uttesh <uttesh@gmail.com>
2020-08-17 22:33:13 +0530

    added generate blank parameters

--- js/chaos_api/param.utility.service.js
+++ js/chaos_api/param.utility.service.js
@@ -6,10 +6,20 @@ class ParamUtilityService {
   }
 
   getBlankString(type) {
-    console.log("from abstration : ", configService.getParamLength("small"));
-    //let length = Math.floor(Math.random() * 1000) + 1;
+    let length =
+      Math.floor(Math.random() * configService.getParamLength(type)) + 1;
+    return this.populateValues(length, " ");
+  }
+
+  populateValues(length, value) {
+    let data = "";
+    for (let i = 0; i < length; i++) {
+      data = value + data;
+    }
+    return data;
   }
 }
 
 let paramTest = new ParamUtilityService(new ConfigService());
-paramTest.getBlankString();
+let blankWord = paramTest.getBlankString("hulk");
+console.log("blankWord: length: ", blankWord.length);
