6116ec1aceb4ece2c06b47d1644cf7998493c700
uttesh <uttesh@gmail.com>
2020-08-26 21:04:15 +0530

    added json parser

--- js/chaos_api/http.parser.service.js
+++ js/chaos_api/http.parser.service.js
@@ -3,22 +3,21 @@ class HttpParserService {
   constructor() {}
 
   async parsePostManJson(postmanFile) {
-    let jsonData = await this.readPostmanFile(postmanFile);
-    console.log("jsonData :: ", jsonData);
-    let postManObject = JSON.parse(jsonData);
-    console.log(":: postManObject ::", postManObject);
+    await this.processJSONFile(postmanFile);
   }
 
-  async readPostmanFile(filePath) {
+  async processJSONFile(filePath) {
     console.log("filePath:: ", filePath);
-    return fs.readFile(filePath, async (err, data) => {
-      console.log("data :: ", data);
-      return data;
+    fs.readFile(filePath, async (err, data) => {
+      if (data) {
+        let postManObject = JSON.parse(data);
+        console.log(":: postManObject ::", postManObject);
+      }
     });
   }
 }
 
 let httpParserService = new HttpParserService();
 httpParserService.parsePostManJson(
-  "C:dev\\clients\\me\\coding_is_infinite_learning\\js\\chaos_api\\dit.postman_collection.json"
+  "C:\\dev\\clients\\me\\coding_is_infinite_learning\\js\\chaos_api\\dit.postman_collection.json"
 );
